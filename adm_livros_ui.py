52389828c7dac59be01dd545ec64e3e0bedb079f
TiaMyllena <myllenah2o@gmail.com>
2019-08-14 14:05:32 -0300

    Nomes das variáveis alterados

---
+++ adm_livros_ui.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'adm_livros.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(900, 550)
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(70, -10, 181, 171))
+        self.pushButton.setStyleSheet("QPushButton{\n"
+"border: None;\n"
+"}")
+        self.pushButton.setText("")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("PiltoverLibrary/Icon/logo.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton.setIcon(icon)
+        self.pushButton.setIconSize(QtCore.QSize(128, 128))
+        self.pushButton.setObjectName("pushButton")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(320, 50, 341, 31))
+        font = QtGui.QFont()
+        font.setFamily("DejaVu Sans")
+        font.setPointSize(24)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.lineEdit_buscar = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_buscar.setGeometry(QtCore.QRect(320, 160, 190, 25))
+        self.lineEdit_buscar.setObjectName("lineEdit_buscar")
+        self.pushButton_buscar = QtWidgets.QPushButton(Dialog)
+        self.pushButton_buscar.setGeometry(QtCore.QRect(530, 160, 80, 25))
+        self.pushButton_buscar.setText("")
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("PiltoverLibrary/Icon/magnifier.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton_buscar.setIcon(icon1)
+        self.pushButton_buscar.setObjectName("pushButton_buscar")
+        self.textBrowser_info = QtWidgets.QTextBrowser(Dialog)
+        self.textBrowser_info.setGeometry(QtCore.QRect(300, 240, 256, 192))
+        self.textBrowser_info.setObjectName("textBrowser_info")
+        self.pushButton_atualizar = QtWidgets.QPushButton(Dialog)
+        self.pushButton_atualizar.setGeometry(QtCore.QRect(300, 470, 80, 25))
+        self.pushButton_atualizar.setObjectName("pushButton_atualizar")
+        self.pushButton_cadastrar = QtWidgets.QPushButton(Dialog)
+        self.pushButton_cadastrar.setGeometry(QtCore.QRect(469, 470, 91, 25))
+        self.pushButton_cadastrar.setObjectName("pushButton_cadastrar")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "Administrar livros"))
+        self.pushButton_atualizar.setText(_translate("Dialog", "Atualizar"))
+        self.pushButton_cadastrar.setText(_translate("Dialog", "Cadastrar livro"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
